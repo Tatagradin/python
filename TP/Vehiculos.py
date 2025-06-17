8603e5315045bdff3dea4e8ce389bb3167fbda2d
manuprieto1234 <mprietosalazar@itba.edu.ar>
2025-06-16 23:57:09 -0300

    Se modifico clase vehiculos, especificamente la logica del costo de las subclases automotor y maritimo

--- TP/Vehiculos.py
+++ TP/Vehiculos.py
@@ -8,39 +8,72 @@ class Vehiculo:
         self.costo_kg = costo_kg          # por kg
 
 
-# Subclases corregidas según los criterios definitivos
-
 class Ferroviario(Vehiculo):
     def __init__(self):
-        super().__init__("Ferroviaria", 100, 150000, 100, None, 3)  # todo fijo excepto costo por km
+        super().__init__("Ferroviaria", 100, 150000, 100, None, 3)
 
     def calcular_costo_por_km(self, distancia):
+        # Según la tabla: $20 para distancias < 200km, $15 para >= 200km
         return 20 if distancia < 200 else 15
 
 
 class Automotor(Vehiculo):
     def __init__(self):
-        super().__init__("Automotor", 80, 30000, 30, 5, None)  # todo fijo excepto costo por kg
+        super().__init__("Automotor", 80, 30000, 30, 5, None)
 
-    def calcular_costo_por_kg(self, peso):
-        return 1 if peso < 15000 else 2
+    def calcular_costo_por_kg(self, peso_total):
+        """
+        Calcula el costo por kg dinámicamente según el peso total de la carga.
+        
+        Lógica:
+        - Cada vehículo tiene capacidad de 30000 kg
+        - Los primeros vehículos completos (30000 kg) se cobran a $2.00/kg
+        - El último vehículo parcial se cobra a $1.00/kg
+        
+        Args:
+            peso_total: Peso total de la carga en kg
+            
+        Returns:
+            float: Costo promedio ponderado por kg
+        """
+        if peso_total <= 0:
+            return 0
+            
+        capacidad_vehiculo = self.capacidad  # 30000 kg
+        
+        # Calcular cuántos vehículos completos necesitamos
+        vehiculos_completos = peso_total // capacidad_vehiculo
+        peso_restante = peso_total % capacidad_vehiculo
+        
+        # Costo para vehículos completos (a $2.00/kg)
+        costo_vehiculos_completos = vehiculos_completos * capacidad_vehiculo * 2.0
+        
+        # Costo para el vehículo parcial (a $1.00/kg)
+        costo_vehiculo_parcial = peso_restante * 1.0 if peso_restante > 0 else 0
+        
+        # Costo total
+        costo_total = costo_vehiculos_completos + costo_vehiculo_parcial
+        
+        # Retornar costo promedio por kg
+        return costo_total / peso_total
 
 
 class Maritimo(Vehiculo):
     def __init__(self):
-        super().__init__("Fluvial", 40, 100000, None, 15, 2)  # todo fijo excepto costo fijo
+        super().__init__("Fluvial", 40, 100000, None, 15, 2.0)
 
     def calcular_costo_fijo(self, tipo):
+        # Según la tabla: $500 para fluvial, $1500 para marítimo
         if tipo == "fluvial":
             return 500
         elif tipo == "maritimo":
             return 1500
-        return 0
+        return 500  # Default a fluvial
 
 
 class Aereo(Vehiculo):
     def __init__(self):
-        super().__init__("Aerea", 600, 5000, 750, 40, 10)  # todo fijo excepto velocidad
+        super().__init__("Aerea", 600, 5000, 750, 40, 10)
 
     def calcular_velocidad(self, prob_mal_tiempo=None):
         if prob_mal_tiempo is not None and prob_mal_tiempo > 0:
